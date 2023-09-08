da9d82c2c2c359e3c03ad8f2929106d4d6633165
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-09-08 23:29:01 +0530

    Add files via upload

---
+++ Column.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Column() {
+    const items=[]
+  return (
+    <React.Fragment>{
+        items.map(items =>(
+            <React.Fragment key={items.id}>
+                <h1>Title</h1>
+                <p>{items.title}</p>
+            </React.Fragment>
+        ))
+    }
+        <td>Name</td>
+        <td>Class</td>
+    </React.Fragment>
+  )
+}
+
+export default Column
