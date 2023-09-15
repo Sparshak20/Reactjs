c7645cbbe59f633ec6327bb943d54fd93f406cb1
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-09-15 23:56:36 +0530

    Add files via upload

---
+++ FragmentDemo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FragmentDemo() {
+  return (
+    <React.Fragment>
+    <h1>FragmentDemo</h1>
+    <p>Lorem20200102000100</p>
+    </React.Fragment>
+  )
+}
+
+export default FragmentDemo
