af1f4d35162be1196cb4cf091aa60998aff64059
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-09-20 23:52:49 +0530

    Add files via upload

---
+++ Inline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const heading={
+    fontSize:'20px',
+    color:'blue'
+}
+function Inline() {
+  return (
+    <div>
+        <h5 style={heading}>Inline</h5>
+    </div>
+  )
+}
+
+export default Inline
